28904f924c6028c575eab0e51641c08c5ef09e09
vadrille <109307947+vadrille12@users.noreply.github.com>
2023-06-21 18:34:48 +0300

    add routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
-export const App = () => {
+import { lazy, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+
+// import { PublicRoute } from 'components/PublicRoute';
+// import { PrivateRoute } from 'components/PrivateRoute';
+// import { MainLayout } from 'components/MainLayout/MainLayout';
+
+// const MainPage = lazy(() => import('../../pages/MainPage'));
+// const AccountPage = lazy(() => import('../../pages/AccountPage'));
+// const CalendarPage = lazy(() => import('../../pages/CalendarPage'));
+// const StatisticsPage = lazy(() => import('../../pages/StatisticsPage'));
+// const LoginPage = lazy(() => import('../../pages/LoginPage'));
+// const RegisterPage = lazy(() => import('../../pages/RegisterPage'));
+// const NotFoundPage = lazy(() => import('../../pages/NotFoundPage'));
+
+export function App() {
+  // const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   dispatch(refresh());
+  // }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      GooseTrack
-    </div>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<MainPage />} />
+
+        <Route element={<PublicRoute />}>
+          <Route path="login" element={<LoginPage />} />
+          <Route path="register" element={<RegisterPage />} />
+        </Route>
+
+        <Route element={<PrivateRoute />}>
+          <Route path="account" element={<AccountPage />} />
+          <Route path="calendar" element={<CalendarPage />}>
+            <Route path="month" element={} />
+            <Route path="month/:month" element={} />
+            <Route path="day" element={} />
+            <Route path="day/:date" element={} />
+          </Route>
+          <Route path="statistics" element={<StatisticsPage />} />
+        </Route>
+
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
   );
-};
+}
