68271cbe4fdb152c7d0b845c77bb6d6678c14e08
MelnykY <106350059+MelnykY@users.noreply.github.com>
2023-06-22 23:53:08 +0300

    Update MainPage.jsx

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import { AuthSection } from 'components/MainPageComponents/AuthSection/AuthSection';
+import { Description } from 'components/MainPageComponents/DescSection/Desc';
 
 export default function MainPage() {
   return (
     <>
       <AuthSection />
+      <Description />
     </>
   );
 }
