412b047ec792b42f11b5859c3217dee634cda635
MelnykY <106350059+MelnykY@users.noreply.github.com>
2023-06-22 23:53:26 +0300

    Update AuthSection.jsx

--- src/components/MainPageComponents/AuthSection/AuthSection.jsx
+++ src/components/MainPageComponents/AuthSection/AuthSection.jsx
@@ -1,38 +1,59 @@
 import React from 'react';
-
+import { ReactComponent as IconLogIn } from 'images/logIn.svg';
+import { useAdaptiveImage } from '../../../hooks/useAdaptiveImage';
 import {
-  Header,
+  ContainerHero,
   ContentWrapper,
   ImageWrapper,
-  LoginLink,
-  RegisterLink,
+  LinkLogin,
+  LinkRegister,
 } from './AuthSection.styled';
 
-import goose1x from 'images/mainPage/desktop/desktop_goose_mainPage.png';
-import goose2x from 'images/mainPage/desktop/desktop_goose_mainPage@2x.png';
-
 export const AuthSection = () => {
+  const { isRetina, isMobile, isTablet, isDesktop } = useAdaptiveImage();
 
   return (
-    <Header>
+    <ContainerHero>
       <ContentWrapper>
         <ImageWrapper>
-          <img
-            srcSet={`${goose1x} 1x, ${goose2x} 2x`}
-            src={goose1x}
-            alt="Goose welcome you"
-            title="Goose welcome you"
-          />
+          {isMobile && (
+            <img
+              src={
+                isRetina
+                  ? require('../../../images/mainPage/mobile/mobile_goose_mainPage@2x.png')
+                  : require('../../../images/mainPage/mobile/mobile_goose_mainPage.png')
+              }
+              alt="the goose welcomes you"
+              width="142"
+              height="142"
+            />
+          )}
+          {(isTablet || isDesktop) && (
+            <img
+              src={
+                isRetina
+                  ? require('../../../images/mainPage/desktop/desktop_goose_mainPage@2x.png')
+                  : require('../../../images/mainPage/desktop/desktop_goose_mainPage.png')
+              }
+              alt="the goose welcomes you"
+              width="150"
+              height="149"
+            />
+          )}
         </ImageWrapper>
-
         <h1>
           G<span>oo</span>seTrack
         </h1>
         <nav>
-          <LoginLink to="login">Log in</LoginLink>
-          <RegisterLink to="register">Sign up</RegisterLink>
+          <LinkLogin to="login">
+            Log in{' '}
+            <IconLogIn
+              style={{ width: '13px', height: '13px', marginLeft: '8px' }}
+            />
+          </LinkLogin>
+          <LinkRegister to="register">Sign up</LinkRegister>
         </nav>
       </ContentWrapper>
-    </Header>
+    </ContainerHero>
   );
 };
