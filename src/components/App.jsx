e309d592d77a140f7235c16ddfbd85f58cde2d4c
vadrille <109307947+vadrille12@users.noreply.github.com>
2023-06-22 13:58:52 +0300

    update routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,18 @@
-// import { lazy, useEffect } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { Route, Routes } from 'react-router-dom';
-
-import { Container } from 'Styles/Container';
-import { ChoosedDay } from './ChoosedDay/ChoosedDay';
-import { GlobalStyle } from 'Styles/GlobalStyle';
+import { lazy, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
 
 // import { PublicRoute } from 'components/PublicRoute';
 // import { PrivateRoute } from 'components/PrivateRoute';
-// import { MainLayout } from 'components/MainLayout/MainLayout';
+import { MainLayout } from 'components/MainLayout/MainLayout';
 
-// const MainPage = lazy(() => import('../../pages/MainPage'));
-// const AccountPage = lazy(() => import('../../pages/AccountPage'));
-// const CalendarPage = lazy(() => import('../../pages/CalendarPage'));
-// const StatisticsPage = lazy(() => import('../../pages/StatisticsPage'));
-// const LoginPage = lazy(() => import('../../pages/LoginPage'));
-// const RegisterPage = lazy(() => import('../../pages/RegisterPage'));
-// const NotFoundPage = lazy(() => import('../../pages/NotFoundPage'));
+const MainPage = lazy(() => import('pages/MainPage/MainPage'));
+// const AccountPage = lazy(() => import('pages/AccountPage'));
+const CalendarPage = lazy(() => import('pages/CalendarPage/CalendarPage'));
+// const StatisticsPage = lazy(() => import('pages/StatisticsPage'));
+const LoginPage = lazy(() => import('pages/LoginPage/LoginPage'));
+const RegisterPage = lazy(() => import('pages/RegisterPage/RegisterPage'));
+// const NotFoundPage = lazy(() => import('pages/NotFoundPage'));
 
 export function App() {
   // const dispatch = useDispatch();
@@ -26,32 +22,28 @@ export function App() {
   // }, [dispatch]);
 
   return (
-    // <Routes>
-    //   <Route path="/" element={<MainLayout />}>
-    //     <Route index element={<MainPage />} />
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<MainPage />} />
 
-    //     <Route element={<PublicRoute />}>
-    //       <Route path="login" element={<LoginPage />} />
-    //       <Route path="register" element={<RegisterPage />} />
-    //     </Route>
+        {/* <Route element={<PublicRoute />}> */}
+        <Route path="login" element={<LoginPage />} />
+        <Route path="register" element={<RegisterPage />} />
+        {/* </Route> */}
 
-    //     <Route element={<PrivateRoute />}>
-    //       <Route path="account" element={<AccountPage />} />
-    //       <Route path="calendar" element={<CalendarPage />}>
-    //         <Route path="month" element={null} />
-    //         <Route path="month/:month" element={null} />
-    //         <Route path="day" element={null} />
-    //         <Route path="day/:date" element={null} />
-    //       </Route>
-    //       <Route path="statistics" element={<StatisticsPage />} />
-    //     </Route>
+        {/* <Route element={<PrivateRoute />}> */}
+        {/* <Route path="account" element={<AccountPage />} /> */}
+        <Route path="calendar" element={<CalendarPage />}>
+          {/* <Route path="month" element={null} />
+            <Route path="month/:month" element={null} />
+            <Route path="day" element={null} />
+            <Route path="day/:date" element={null} /> */}
+        </Route>
+        {/* <Route path="statistics" element={<StatisticsPage />} /> */}
+        {/* </Route> */}
 
-    //     <Route path="*" element={<NotFoundPage />} />
-    //   </Route>
-    // </Routes>
-    <Container>
-      <ChoosedDay />
-      <GlobalStyle />
-    </Container>
+        {/* <Route path="*" element={<NotFoundPage />} /> */}
+      </Route>
+    </Routes>
   );
 }
---
+++ src/components/MainLayout/MainLayout.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router';
+
+export const MainLayout = () => {
+  return (
+    <>
+      <header>
+        Header
+      </header>
+      <Outlet />
+    </>
+  );
+};
