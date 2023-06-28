f0aff8842b10f37b60b2e46eb7c566d9a448ba02
Oleksandr Makogin <sashamakohin@gmail.com>
2023-06-28 16:26:09 +0300

    added statistics redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -13,6 +13,7 @@ import storage from 'redux-persist/lib/storage';
 
 import { authSlice } from './auth/authSlice';
 import { reviewReducer } from './reviews/reviewSlice';
+import { statisticsReducer } from './statistics/statisticsSlice';
 
 const authPersistConfig = {
   key: 'auth',
@@ -24,6 +25,7 @@ export const store = configureStore({
   reducer: {
     auth: persistReducer(authPersistConfig, authSlice.reducer),
     reviews: reviewReducer,
+    statistics: statisticsReducer,
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
