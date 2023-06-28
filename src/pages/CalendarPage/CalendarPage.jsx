c2a6445c8e7ac1439c047a4686e6f35c66d137a7
Yaroslav12002 <my.mail12002@gmail.com>
2023-06-28 22:35:44 +0300

    Calendar page remarks removed

--- src/pages/CalendarPage/CalendarPage.jsx
+++ src/pages/CalendarPage/CalendarPage.jsx
@@ -4,8 +4,6 @@ import { Outlet } from 'react-router-dom';
 import { Suspense, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchTasks } from 'redux/tasks/operations';
-// import { useLocation, useNavigate, useParams } from 'react-router';
-// import moment from 'moment';
 
 export default function CalendarPage() {
   const dispatch = useDispatch();
@@ -14,44 +12,9 @@ export default function CalendarPage() {
     dispatch(fetchTasks());
   }, [dispatch]);
 
-  // const navigate = useNavigate();
-
-  // const { currentDay, currentDate } = useParams();
-  // const { pathname } = useLocation();
-  // const typeSelect = pathname.includes('/calendar/day') ? 'day' : 'month';
-
-  // const workDate = currentDate || currentDay;
-
-  // useEffect(() => {
-  //   if (pathname === '/calendar' || pathname === '/calendar/') {
-  //     navigate(`/calendar/month/${moment().format('YYYY-MM-DD')}`);
-  //   }
-  // }, [pathname, navigate]);
-
-  // const prevDayHandler = () => {
-  //   navigate(
-  //     `/calendar/${typeSelect}/${moment(workDate)
-  //       .subtract(1, typeSelect)
-  //       .format('YYYY-MM-DD')}`
-  //   );
-  // };
-
-  // const nextDayHandler = () => {
-  //   navigate(
-  //     `/calendar/${typeSelect}/${moment(workDate)
-  //       .add(1, typeSelect)
-  //       .format('YYYY-MM-DD')}`
-  //   );
-  // };
-
   return (
     <CalendarPageContainer>
-      <CalendarToolBar
-      // today={moment(workDate)}
-      // typeSelect={typeSelect}
-      // prevDayHandler={prevDayHandler}
-      // nextDayHandler={nextDayHandler}
-      />
+      <CalendarToolBar />
       <Suspense>
         <Outlet />
       </Suspense>
