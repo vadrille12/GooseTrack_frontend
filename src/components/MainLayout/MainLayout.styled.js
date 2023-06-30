e373ce6e9193755419a8fc0061e63887e7991d1d
SergeiTkachenko <fox.sergei92@gmail.com>
2023-06-30 14:57:46 +0200

    fix shale

--- src/components/MainLayout/MainLayout.styled.js
+++ src/components/MainLayout/MainLayout.styled.js
@@ -228,6 +228,8 @@ export const Container = styled.div`
   display: flex;
   min-height: 100vh;
   width: 100%;
+  justify-content: center;
+
   @media screen and (min-width: 1440px) {
     width: 1150px;
   }
