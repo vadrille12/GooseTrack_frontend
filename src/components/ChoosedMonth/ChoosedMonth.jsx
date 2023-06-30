1c2e25ba9c86ddf5134f60236ecdbc76f293a7c4
Yaroslav12002 <my.mail12002@gmail.com>
2023-06-30 22:46:00 +0300

    fix spiner for calendar

--- src/components/ChoosedMonth/ChoosedMonth.jsx
+++ src/components/ChoosedMonth/ChoosedMonth.jsx
@@ -1,27 +1,16 @@
 import { useSelector } from 'react-redux';
 
-import {
-  selectTasks,
-  selectIsLoading,
-  selectError,
-} from '../../redux/tasks/selectors';
+import { selectTasks } from '../../redux/tasks/selectors';
 import { MonthCalendarHead } from './MonthCalendarHead/MonthCalendarHead';
 import { CalendarTable } from './CalendarTable/CalendarTable';
-import Spinner from 'components/Spinner/spinner';
 
 const ChoosedMonth = () => {
   const tasks = useSelector(selectTasks);
-  const isLoading = useSelector(selectIsLoading);
-  const error = useSelector(selectError);
 
   return (
     <>
-      {(isLoading && !error && <Spinner />) || (
-        <>
-          <MonthCalendarHead />
-          <CalendarTable tasks={tasks} />
-        </>
-      )}
+      <MonthCalendarHead />
+      <CalendarTable tasks={tasks} />
     </>
   );
 };
--- src/pages/CalendarPage/CalendarPage.jsx
+++ src/pages/CalendarPage/CalendarPage.jsx
@@ -2,11 +2,15 @@ import { CalendarToolBar } from 'components/CalendarToolBar/CalendarToolBar';
 import { CalendarPageContainer } from './CalendarPage.styled';
 import { Outlet } from 'react-router-dom';
 import { Suspense, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchTasks } from 'redux/tasks/operations';
+import { selectIsLoading, selectError } from '../../redux/tasks/selectors';
+import Spinner from 'components/Spinner/spinner';
 
 export default function CalendarPage() {
   const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
 
   useEffect(() => {
     dispatch(fetchTasks());
@@ -15,9 +19,11 @@ export default function CalendarPage() {
   return (
     <CalendarPageContainer>
       <CalendarToolBar />
-      <Suspense>
-        <Outlet />
-      </Suspense>
+      {(isLoading && !error && <Spinner />) || (
+        <Suspense>
+          <Outlet />
+        </Suspense>
+      )}
     </CalendarPageContainer>
   );
 }
