7cbccb6cbcc7845e553102f81bccab69cb33a635
vadrille <109307947+vadrille12@users.noreply.github.com>
2023-07-02 01:37:09 +0300

    Revert "done 404 page"

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,33 +1,7 @@
-import { Text, Main, Info, Goose } from './NotFoundPage.styled';
-
 export default function NotFoundPage() {
   return (
-    <Main>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          textAlign: 'center',
-        }}
-      >
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            marginBottom: '30px',
-          }}
-        >
-          <Text>4</Text>
-          <Goose />
-          <Text>4</Text>
-        </div>
-        <Info>
-          We're sorry, the page you requested could not be found. Please go back
-          to the homepage.
-        </Info>
-      </div>
-    </Main>
+    <main>
+      <b style={{ textAlign: 'center' }}>404. Page not found</b>
+    </main>
   );
 }
