cd6ca768797a5a0e6ae818b972caecece3364f8f
vadrille12 <109307947+vadrille12@users.noreply.github.com>
2023-07-02 19:57:18 +0000

    Deploying to gh-pages from @ vadrille12/GooseTrack_frontend@a162c8d45751b9728c69b362ce620eae6105c5cf 🚀

---
+++ static/js/820.61479cd3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkGooseTrack_frontend=self.webpackChunkGooseTrack_frontend||[]).push([[820],{78820:function(t,n,c){c.d(n,{V9Z:function(){return r}});var e=c(89983);function r(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 1024 1024"},child:[{tag:"path",attr:{d:"M946.5 505L534.6 93.4a31.93 31.93 0 0 0-45.2 0L77.5 505c-12 12-18.8 28.3-18.8 45.3 0 35.3 28.7 64 64 64h43.4V908c0 17.7 14.3 32 32 32H448V716h112v224h265.9c17.7 0 32-14.3 32-32V614.3h43.4c17 0 33.3-6.7 45.3-18.8 24.9-25 24.9-65.5-.1-90.5z"}}]})(t)}}}]);
+//# sourceMappingURL=820.61479cd3.chunk.js.map
